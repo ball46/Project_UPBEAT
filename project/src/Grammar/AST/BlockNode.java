9c5c8174e958fd1bd9a8645ffd2b2b51abd6df2f
i am doraemon <khunball46@gmail.com>
2023-02-07 11:03:17 +0700

    AST is finish

--- project/src/Grammar/AST/BlockNode.java
+++ project/src/Grammar/AST/BlockNode.java
@@ -2,7 +2,7 @@ package Grammar.AST;
 import java.util.List;
 
 public class BlockNode implements Nodes{
-    List<Nodes> statements;
+    private List<Nodes> statements;
     @Override
     public void evaluate() {
         System.out.println("BlockNode is evaluated");
