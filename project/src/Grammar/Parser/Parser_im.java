c2141f5dd984750c07f5610d13d9a1a6fafd0c3d
i am doraemon <khunball46@gmail.com>
2023-02-12 19:40:45 +0700

    AST is finish

--- project/src/Grammar/Parser/Parser_im.java
+++ project/src/Grammar/Parser/Parser_im.java
@@ -197,7 +197,7 @@ public class Parser_im implements Parser {/*
     }
 
     private ExprNode parseInfoExpression() {
-        if(tkz.peek("oppnent")){
+        if(tkz.peek("opponent")){
             tkz.consume();
             return new OpponentNode();
         }else if(tkz.peek("nearby")){
