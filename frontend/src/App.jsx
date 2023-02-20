622adb673f72b635784998a09d9335dd60feefa7
gamxu <wuttipat.did@gmail.com>
2023-02-20 23:12:01 +0700

    created map

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import HexGrid from "./components/Grid/HexGrid";
+
+
+function App() {
+  return (
+    <div class="container">
+      <HexGrid />
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/components/Row/HexagonRow.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "./Hexstyle.css";
+
+function HexagonRow(props) {
+  const tags = [];
+  let amount = 13;
+  for (let i = 0; i < amount; i++) {
+    tags.push(
+      <div class="hexagon">
+        <div class="content">{props.x},{i+1}</div>
+      </div>
+    );
+  }
+  return <div class="row">{tags}</div>;
+}
+
+export default HexagonRow;
