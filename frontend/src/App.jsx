448efbb0eceb1bbdfa0611c04d08247d58ee93d7
gamxu <wuttipat.did@gmail.com>
2023-02-26 20:41:47 +0700

    restore router

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,10 +9,10 @@ function App() {
     <Router>
       <Routes>
         <Route path="/" element={<Landing />}></Route>
-        <Route path="/input" element={<InputName/>}></Route>
+        <Route path="/input" element={<InputName />}></Route>
+        <Route path="/map" element={<HexGrid />}></Route>
       </Routes>
     </Router>
-    // <InputName/>
   );
 }
 
